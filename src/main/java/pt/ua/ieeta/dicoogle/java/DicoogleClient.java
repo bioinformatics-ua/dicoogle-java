75ace8155cfccc87b0618c72e57749c8994c517f
Luís A. Bastião Silva <luis.kop@gmail.com>
2013-05-28 01:28:07 +0100

    Added Dicoogle API

---
+++ src/main/java/pt/ua/ieeta/dicoogle/java/DicoogleClient.java
@@ -0,0 +1,49 @@
+/**
+ *
+ * Copyright (C) 2013 - Luís A. Bastião Silva and Universidade de Aveiro This
+ * program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pt.ua.ieeta.dicoogle.java;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ *
+ * @author bastiao
+ */
+public class DicoogleClient implements IDicoogleClient{
+
+    public List<String> searchFreeText(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<String> searchAdvanced(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<String> dump(String sopInstanceUID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void download(String sopInstanceUID, File dest) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int numberOfResults(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
