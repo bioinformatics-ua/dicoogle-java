0c59863548f4e12123c4123bbb3b14d35cdb7069
Luís A. Bastião Silva <luis.kop@gmail.com>
2013-05-28 01:52:08 +0100

    Added restlet endpoints

---
+++ src/main/java/pt/ua/ieeta/dicoogle/java/EndPoints.java
@@ -0,0 +1,42 @@
+/**
+ *
+ * Copyright (C) 2013 - Luís A. Bastião Silva and Universidade de Aveiro This
+ * program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package pt.ua.ieeta.dicoogle.java;
+
+/**
+ *
+ * @author bastiao
+ */
+public class EndPoints 
+{
+
+    public static final String SEARCH = "search";
+    public static final String DIC2PNG = "dic2png";
+    public static final String DICTAGS = "dictags";
+    public static final String PLUGINS = "plugin";
+    public static final String INDEX = "indexer";
+    public static final String DIM = "dim";
+    public static final String FILE = "file";
+    public static final String DUMP = "dump";
+    public static final String TAGS = "tags";
+    public static final String IMAGE = "image";
+    public static final String ENUMFIELD = "enumField";
+    public static final String WADO = "wado";
+    public static final String EXAMTIME = "examTime";
+            
+}
